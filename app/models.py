528baac011a5c96c21e430348ee9e31b7d194f96
Douglas Lenz <thedouglenz@gmail.com>
2024-11-08 17:16:19 -0500

    hash and write passwords to db

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from passlib.apps import custom_app_context as pwd_context
+
 from . import db
 
 class UserModel(db.Model):
@@ -5,9 +7,14 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
-    deleted = db.Column(db.Boolean, nullable=False)
+    password_hash = db.Column(db.String(128))
+    deleted = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f"User(name = {self.name}, email = {self.email})"
 
-    
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
--- app/resources.py
+++ app/resources.py
@@ -5,12 +5,13 @@ from .models import UserModel, db
 user_args = reqparse.RequestParser()
 user_args.add_argument('name', type=str, required=True, help="Name cannot be blank")
 user_args.add_argument('email', type=str, required=True, help="Email cannot be blank")
+user_args.add_argument('password', type=str)
 
 # Define JSON structure for marshal
 userFields = {
     'id': fields.Integer,
     'name': fields.String,
-    'email': fields.String
+    'email': fields.String,
 }
 
 class UsersResource(Resource):
@@ -22,21 +23,23 @@ class UsersResource(Resource):
     @marshal_with(userFields)
     def post(self):
         args = user_args.parse_args()
+        if not args['password']:
+            abort(400, message="New user registration requires password")
         user = UserModel(
             name = args['name'],
             email = args['email']
         )
+        user.hash_password(args['password'])
         db.session.add(user)
         db.session.commit()
-        users = UserModel.query.all()
-        return users, 201
+        return user, 201
 
 class UserResource(Resource):
     @marshal_with(userFields)
     def get(self, id):
         user = UserModel.query.filter_by(id=id).first()
         if not user:
-            abort(404, "User not found")
+            abort(404, message="User not found")
         return user
 
     @marshal_with(userFields)
@@ -44,8 +47,7 @@ class UserResource(Resource):
         args = user_args.parse_args()
         user = UserModel.query.filter_by(id=id).first()
         if not user:
-            abort(404, "User not found")
-
+            abort(404, message="User not found")
         user.name = args["name"]
         user.email = args["email"]
         db.session.commit()
@@ -55,7 +57,7 @@ class UserResource(Resource):
     def delete(self, id):
         user = UserModel.query.filter_by(id=id).first()
         if not user:
-            abort(404, "User not found")
+            abort(404, message="User not found")
         db.session.delete(user)
         user.deleted = True
         db.session.commit()
