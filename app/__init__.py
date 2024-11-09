aaac4b236da54e67a9f7b41bdf191aaea8fec97d
Douglas Lenz <thedouglenz@gmail.com>
2024-11-09 06:46:33 -0500

    env and db setup

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
